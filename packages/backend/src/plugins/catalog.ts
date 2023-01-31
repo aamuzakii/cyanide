9db6d2f4abc6b5cd494a1b5ab919d5c8efbafac9
@abdullah.muzaki <abdullah.muzaki@aruna.id>
2023-01-31 17:39:34 +0700

    POC Gitlab dashboard

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -1,5 +1,6 @@
 import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
 import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backend';
+import { GitlabFillerProcessor } from '@immobiliarelabs/backstage-plugin-gitlab-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
 
@@ -8,6 +9,8 @@ export default async function createPlugin(
 ): Promise<Router> {
   const builder = await CatalogBuilder.create(env);
   builder.addProcessor(new ScaffolderEntitiesProcessor());
+
+  builder.addProcessor(new GitlabFillerProcessor(env.config));
   const { processingEngine, router } = await builder.build();
   await processingEngine.start();
   return router;
